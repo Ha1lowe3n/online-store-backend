11ef47f66d40be72f4e37861463f295e0d0c034b
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-06-27 17:05:39 +0800

    add routers

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const express = require("express");
 require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 
-const sequelize = require("./db.js");
+const sequelize = require("./db");
 const models = require("./models/models"); //  подключаем модели в приложение (создаются таблицы в pgAdmin)
+const router = require("./routes/index");
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
+app.options("*", cors());
+app.use(express.json());
+app.use("/api", router);
 
 const start = async () => {
     try {
         await sequelize.authenticate(); // connect to db
         await sequelize.sync(); // сверяет соястояние бд со схемой данных, которые мы описываем
-        app.listen(() => {
+        app.listen(PORT, () => {
             console.log(`Server is working on port ${PORT}`);
         });
     } catch (e) {
