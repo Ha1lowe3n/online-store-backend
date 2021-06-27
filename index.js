8762b1993bf541f636ec58b63fcc90162c29cb64
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-06-27 16:04:14 +0800

    add models and relationships иуеÑbetween models

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 require("dotenv").config();
 
 const sequelize = require("./db.js");
+const models = require("./models/models"); //  подключаем модели в приложение (создаются таблицы в pgAdmin)
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
