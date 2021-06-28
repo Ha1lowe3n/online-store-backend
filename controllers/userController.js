1c5476876f4909934d13c086c47278509b06d946
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-06-28 15:57:43 +0800

    end User, add authorization, login with jwt token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,82 @@
+const bcrypt = require("bcrypt");
+const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+
 const ApiError = require("../error/ApiError");
+const { User, Basket } = require("../models/models");
+
+const generateJwt = (id, email, role) => {
+    return jwt.sign({ id, email, role }, process.env.SECRET_KEY, {
+        expiresIn: "24h",
+    });
+};
 
 class UserController {
-    async registration(req, res) {}
+    async registration(req, res) {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res
+                    .status(400)
+                    .json({ message: "Ошибка при регистрации", errors });
+            }
 
-    async login(req, res) {}
+            const { email, password, role } = req.body;
 
-    async check(req, res, next) {
-        const { id } = req.query;
-        if (!id) {
-            return next(ApiError.badRequest("Не задан ID"));
+            const candidate = await User.findOne({ where: { email } });
+            if (candidate) {
+                return res.status(400).json({
+                    message: "Пользователь с таким email уже существует",
+                });
+            }
+
+            const hashPassword = await bcrypt.hashSync(password, 5);
+            const user = await User.create({
+                email,
+                role,
+                password: hashPassword,
+            });
+            const basket = await Basket.create({ userId: user.id });
+            const token = generateJwt(user.id, email, user.role);
+            return res.json({ token });
+        } catch (e) {
+            console.error(e);
+            res.status(400).json({ message: "Registration error" });
         }
-        res.json(id);
+    }
+
+    async login(req, res, next) {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res
+                .status(400)
+                .json({ message: "Ошибка при регистрации", errors });
+        }
+
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ where: { email } });
+        if (!user) {
+            return next(
+                ApiError.internal("Пользователь с таким email не найден")
+            );
+        }
+
+        let comparePassword = bcrypt.compareSync(password, user.password);
+        if (!comparePassword) {
+            return next(
+                ApiError.internal("Пользователь с таким email не найден")
+            );
+        }
+
+        const token = generateJwt(user.id, email, user.role);
+        return res.json({ token });
+    }
+
+    async checkUser(req, res) {
+        const { id, email, role } = req.user;
+        const token = generateJwt(id, email, role);
+        return res.json({ message: "Вы авторизованы" });
     }
 }
 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,13 +1,32 @@
 const Router = require("express");
+const { check } = require("express-validator");
 
 const userController = require("../controllers/userController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 const router = new Router();
 
-const { registration, check, login } = userController;
+const { registration, checkUser, login } = userController;
 
-router.post("/registration", registration);
-router.post("/login", login);
-router.get("/auth", check);
+router.post(
+    "/registration",
+    [
+        check("email", "Почта должна быть указана").notEmpty(),
+        check(
+            "password",
+            "Пароль должен быть больше 4 и меньше 10 символов"
+        ).isLength({ min: 4, max: 10 }),
+    ],
+    registration
+);
+router.post(
+    "/login",
+    [
+        check("email", "Почта должна быть указана").isEmail(),
+        check("password", "Пароль должен быть набран").notEmpty(),
+    ],
+    login
+);
+router.get("/auth", authMiddleware, checkUser);
 
 module.exports = router;
